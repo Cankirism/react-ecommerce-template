515d1794ab6e0c60b20469fecf9d805be23b04bb
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-05-13 08:37:09 +0300

    index.js test page prepared

--- server/index.js
+++ server/index.js
@@ -1,16 +1,4 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const { STATUS_CODES } = require("http");
-const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const { error } = require("console");
-require("dotenv").config();
-const cors = require("cors");
-
-
-
 const app = express();
 // app.use(express.static(path.resolve(__dirname, "./build")));
 // app.use(express.json({limit:'10mb'}));
