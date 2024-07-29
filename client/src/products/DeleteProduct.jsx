18656714597f58bb24c816c262a120b02b18e0c2
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-07-29 13:36:41 +0300

    delete page changed

--- client/src/products/DeleteProduct.jsx
+++ client/src/products/DeleteProduct.jsx
@@ -3,54 +3,55 @@ import { fetchProduct, getTopProducts } from "../api/api";
 import { remove } from "../api/api";
 import { paginateProducts } from "../api/api";
 import { HttpStatusCode } from "axios";
-import toast,{ Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import Loader from "../components/Loader";
+
 const DeleteProduct = () => {
-  const [product, setProduct] = useState({name:"",price:"",tumbrlImage:""});
-  const [isLoading,setIsLoading]=useState(true);
-  const [pid,setPid]=useState("");
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    tumbrlImage: "",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [pid, setPid] = useState("");
+  const [products, setProducts] = useState([]);
   useEffect(async () => {
     // const productList = await fetchProduct(id);
     // setProducts(productList.data);
     // setIsLoading(false);
-    
+    await fetchProducts();
   });
 
-   const deleteProduct= async (id)=>{
-    console.log("delete butonu tıklandı")
+  const deleteProduct = async (id) => {
+    console.log("delete butonu tıklandı");
     const deleteResult = await remove(id);
-  if(deleteResult.status===HttpStatusCode.Ok){
-toast.success("Ürün silindi");
-  }
-  else {
-    toast.error("silerken hata")
-  }
-
-   }
-
-   const getProduct =async (id)=>{
-    if(id==""){
-      toast.error("Lütfen ürün kodunu giriniz")
-
+    if (deleteResult.status === HttpStatusCode.Ok) {
+      toast.success("Ürün silindi");
+    } else {
+      toast.error("silerken hata");
     }
-    else {
+  };
+
+  const getProduct = async (id) => {
+    if (id == "") {
+      toast.error("Lütfen ürün kodunu giriniz");
+    } else {
       const prd = await fetchProduct(id);
-    setProduct(prd.data);
-
+      setProduct(prd.data);
     }
-    
+  };
 
-
-   }
+  const fetchProducts = async () => {
+    const prds = await getTopProducts();
+    setProducts(prds.data);
+  };
 
   return (
     <div className="containerd">
-      <Toaster/>
-      
-      
-        <div className="cartt">
-        
-        <input className="col-sm-12" placeholder="Ürün kodunu giriniz" onChange={(e)=>setPid(e.target.value)}></input>
+      <Toaster />
+
+      <div className="cartt">
+        {/* <input className="col-sm-12" placeholder="Ürün kodunu giriniz" onChange={(e)=>setPid(e.target.value)}></input>
         <button 
         className="btn-sm btn-success"
         onClick={()=>getProduct(pid)}
@@ -79,16 +80,30 @@ toast.success("Ürün silindi");
         ):(<div></div>)
 
           
-        }
+        } */}
 
+        {products.map((prd) => (
+          <div className="productd">
+            <img src={prd.tumbrImage} alt={prd.name}/>
+             <div className="product-info">
+            <h5>{prd.name}</h5>
+
+            {prd.name}
+            <p>Fiyat: {product.price} TL</p>
+            <p></p>
+            <button
+              className="remove-btn btn-sm"
+              onClick={() => deleteProduct(prd._id)}
+            >
+              Sil
+            </button>
+          </div>
+
+          </div>
+         
+        ))}
       </div>
-      
-    
-      
-
     </div>
-    
-    
   );
 };
 export default DeleteProduct;
